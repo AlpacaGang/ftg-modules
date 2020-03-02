f6abe7020c7e94371dfebe39cdf5d5eb517a5be3
PSPshnik <61647101+PSPshnik@users.noreply.github.com>
2020-03-03 00:04:42 +0500

    Update wait.py

--- wait.py
+++ wait.py
@@ -3,6 +3,7 @@ from .. import loader, utils
 import logging
 import datetime
 import time
+import asyncio
 
 logger = logging.getLogger(__name__)
 
@@ -14,18 +15,20 @@ def register(cb):
 @loader.tds
 class WAITMod(loader.Module):
     """Provides a message saying that you are unavailable"""
-    strings = {"name": "wait",
-               "fuck": "FUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCK"}
-    
-    def config_complete(self):
+    strings = {"name": "wait"}
+
+    def __init__(self):
         self.name = self.strings["name"]
+
+    def config_complete(self):
+        pass
 
     async def wait5cmd(self, message):
         """Эта команда удаляет сообхение черезе 5 секунд"""
         await utils.answer(message, "Через 5 секунд это сообщение удалится")
 
         for i in range(4, -1, -1):
-            await time.sleep(1)
+            await asyncio.sleep(1)
             await utils.answer(message, "Через " + str(i) + " секунд это сообщение удалится")
 
         await message.delete()
@@ -41,12 +44,12 @@ class WAITMod(loader.Module):
                 await utils.answer(message, "Через " + str(x) + " секунд это сообщение удалится")
 
                 for i in range(x - 1, -1, -1):
-                    time.sleep(1)
+                    await asyncio.sleep(1)
                     await utils.answer(message, "Через " + str(i) + " секунд это сообщение удалится")
                 await message.delete()
             except:
                 await utils.answer(message, "Вы указали не число!")
-    
+
     async def tagcmd(self, message):
         """Эта команда для троллинга друзей. \nЕй вы можете тегнуть друга, а сообщение само удалится!"""
         await message.delete()
