70c3e7dd3de69fb8d7e693e827116c0b7242f602
PSPshnik <61647101+PSPshnik@users.noreply.github.com>
2020-04-10 16:51:21 +0500

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,29 @@
+from .. import loader, utils
+
+import logging
+import datetime
+import time
+import asyncio
+
+logger = logging.getLogger(__name__)
+
+
+def register(cb):
+    cb(DICEMod())
+
+
+@loader.tds
+class DICEMod(loader.Module):
+    """Кто читает тот гей"""
+    strings = {"name": "dice"}
+
+    def __init__(self):
+        self.name = self.strings["name"]
+
+    def config_complete(self):
+        pass
+
+    async def dicecmd(self, message):
+        """Эта команда для того чтобы кинуть кубик и получить случайное значение от 1 до 6"""
+        await message.delete()
+        await utils.answer(message, "🎲")
