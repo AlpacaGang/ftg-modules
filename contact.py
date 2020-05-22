e39ee64a4524d445749c28000a3ebb7a87cdd03e
PSPshnik <61647101+PSPshnik@users.noreply.github.com>
2020-05-23 00:59:11 +0500

    Update contact.py

--- contact.py
+++ contact.py
@@ -29,14 +29,14 @@ class CONTACTMod(loader.Module):
             await message.delete()
             x = 10
             lst = str(x)
-            await utils.respond(message, lst)
+            await utils.respond(lst)
 
             dd = time.time()
 
             while time.time() - dd < x:
                 now = str(x - round(time.time() - dd))
                 if now != lst:
-                    await utils.respond(message, now)
+                    await utils.respond(now)
                 lst = now
         except:
             await utils.answer(message, "Упс, ошибочка вышла! Напшите @gerasikoff, он вам поможет")
