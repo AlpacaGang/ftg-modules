9ab1bb8c9f805a60d14f6b15902d9c43481cd835
Anton Egorov <a12286@yandex.com>
2020-06-09 00:50:39 +0300

    inactive: added .recount
    It iterates over the whole chat and recounts database.
    It may take a certain amount of time - depends on how many messages are
    there in the chat.
    
    Signed-off-by: Anton Egorov <a12286@yandex.com>

--- inactive.py
+++ inactive.py
@@ -1,6 +1,7 @@
 import logging
+import json
 # import telethon
-from .. import loader, utils
+from .. import loader, utils, security
 
 logger = logging.getLogger(__name__)
 
@@ -13,13 +14,21 @@ class InactiveDetectorMod(loader.Module):
         "top_header": "These {un} users wrote {mn} messages or less since joining the group:\n\n",
         "top_place": "[{name}](tg://user?id={uid}) ({nmsg})",  # FIXME: mentions
         "top_delimiter": ", ",  # TODO: move to config
-        "not_int": "<b>Most messages must be integer</b>"
+        "not_int": "<b>Most messages must be integer</b>",
+        "recount_priv": "<b>I can't recount stats in private messages!</b>",
+        "recount_started": "<b>Processing recount for chat {}. It may take a lot.</b>",
+        "recount_db_dumped": "<b>Dumped database to owners and/or saved messages</b>",
+        "recount_dump": "<b>Database dump for chat {cid}:<b>\n\n<pre>{dmp}</pre>",
+        "recount_iter_done": "<b>Iterated over {} messages in this chat</b>",
+        "recount_finish": "<b>Recount successful!</b>"
     }
 
     def __init__(self):
         self.config = loader.ModuleConfig(
             "default_chat_id", -1001457369532, "Chat ID to get top if command used in PM",
-            "top_delimiter", ', ', "Separates inactivity top members"
+            "top_delimiter", ', ', "Separates inactivity top members",
+            "dump_db_before_recount", False, "Dump database of chat before recounting. "
+                                             "Dump will be sent to saved or bot owners"
         )
         self.name = self.strings['name']
 
@@ -71,6 +80,39 @@ class InactiveDetectorMod(loader.Module):
             + self.config['top_delimiter'].join(text)
 
         await utils.answer(message, msg, parse_mode="md", silent=True)
+
+    async def recountcmd(self, message):
+        if message.is_private:
+            await utils.answer(message, self.strings('recount_priv', message))
+            return
+        limit = message.id
+        chat_id = message.chat_id
+        await utils.answer(message, self.strings('recount_started', message).format(chat_id))
+        db = self.db.get(__name__, str(chat_id), {})
+        json_db = json.dumps(db)
+        msg = self.strings("recount_dump", message).format(cid=chat_id, dmp=json_db)
+        logging.debug('Database dump (chat %d): %s', chat_id, json_db)
+        owners = self.db.get(security.__name__, "owner", ["me"])
+        if owners:
+            for owner in owners:
+                try:
+                    await self.client.send_message(owner, msg)
+                except Exception:
+                    logger.warning("Dump of chat %d sending to %d failed",
+                                   chat_id, owner, exc_info=True)
+        new_db = {}
+        n = 0
+        async for msg in self.client.iter_messages(chat_id, limit=limit + 228):
+            if not msg.sender.bot:
+                n += 1
+                from_id = msg.from_id
+                # Ensure such user exists, or create him
+                new_db[str(from_id)] = new_db.get(str(from_id),
+                                                  self.get_empty_user(msg.sender))
+                new_db[str(from_id)]['cnt'] += 1
+        await utils.answer(message, self.strings("recount_iter_done", message).format(n))
+        self.db.set(__name__, str(chat_id), new_db)
+        await utils.answer(message, self.strings("recount_finish", message))
 
     async def watcher(self, message):
         if message.is_private:
